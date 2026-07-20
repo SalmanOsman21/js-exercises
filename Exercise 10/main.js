1c4b0feceb5eaef479a71eae3060232c03034b39
SalmanOsman21 <salmanosman281@gmail.com>
2026-07-20 19:31:54 +0300

    Exercise 10

---
+++ Exercise 10/main.js
@@ -0,0 +1,16 @@
+let libary=[
+    {
+        title:"The Great Gatsby", Author: "F. Scott Fitzgerald" , Year: 1925
+    },
+    {
+        Title: "To Kill a Mockingbird", Author: "Harper Lee" , Year: 1960
+    },
+    {
+        Title: "1984", Author: "George Orwell",
+        Year: 1949
+        
+    }
+]
+
+console.log(libary[0].title);
+console.log(libary[1].Author);
