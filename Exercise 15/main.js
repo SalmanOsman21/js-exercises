99301f8f30a998c625f1fe298690fa69186da5f2
SalmanOsman21 <salmanosman281@gmail.com>
2026-07-22 08:05:09 +0300

    Exercise 15

---
+++ Exercise 15/main.js
@@ -0,0 +1,13 @@
+const person = [
+    {name: "moha" , age :25 , city:"Wonderland"}, 
+    {name: "bob" , age :23 , city:"Mogdisho"}, 
+    {name: "Ali" , age :15 , city:"Hargeysa"}
+]
+ console.log("Proporties and Value of each person")
+
+for(let p of person){
+    console.log("---")
+    for(let key in p){
+           console.log(key + ": " +p[key]);
+    }
+}
