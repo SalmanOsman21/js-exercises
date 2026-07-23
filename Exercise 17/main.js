294aae079a252f9df0bf87e035f6ddafea2f3d7c
SalmanOsman21 <salmanosman281@gmail.com>
2026-07-23 12:31:16 +0300

    Exercise 17

---
+++ Exercise 17/main.js
@@ -0,0 +1,10 @@
+let temprature= 17;
+if(temprature>0){
+    console.log("Very cold");
+}else if(temprature>15){
+    console.log("Cold")
+}else if(temprature>25){
+    console.log("Warm")
+} else if(temprature>25){
+    console.log("Hot")
+}
