5b7d4349dda18745ff8d4a381af77450b5a4b18d
SalmanOsman21 <salmanosman281@gmail.com>
2026-07-23 17:37:45 +0300

    Exercise 18

--- Exercise 17/main.js
+++ Exercise 17/main.js
@@ -1,10 +1,11 @@
-let temprature= 17;
-if(temprature>0){
+let temperature = 15;
+
+if (temperature < 0) {
     console.log("Very cold");
-}else if(temprature>15){
-    console.log("Cold")
-}else if(temprature>25){
-    console.log("Warm")
-} else if(temprature>25){
-    console.log("Hot")
+} else if (temperature >= 0 && temperature <= 15) {
+    console.log("Cold");
+} else if (temperature > 15 && temperature <= 25) {
+    console.log("Warm");
+} else {
+    console.log("Hot");
 }
---
+++ Exercise 18/main.js
@@ -0,0 +1,5 @@
+let score = 70;
+
+let result = score >= 60 ? "pass" : "Fail";
+
+console.log(result);
