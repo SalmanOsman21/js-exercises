6513f0fa58cc2f49c14075ac09afd7f7b88c7c7a
SalmanOsman21 <salmanosman281@gmail.com>
2026-07-24 21:01:53 +0300

    Update path

--- Exercise 26/main.js
+++ Exercise 26/main.js
@@ -7,6 +7,7 @@ console.log("Start blocking delay");
 console.log(delayBlocking());
 console.log("This message is blocked untill delay is complete");
 
+//Non blockin Asynchronous
 function nonBlockingDelay() {
   setTimeout(() => {
     console.log("Non blocking delay completed after 2s !");
