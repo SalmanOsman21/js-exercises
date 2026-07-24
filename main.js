d3bea332e8ba9d8a6c82ae3ae88c8ddca6af9cbe
SalmanOsman21 <salmanosman281@gmail.com>
2026-07-24 20:47:19 +0300

    Exercise 26

---
+++ main.js
@@ -0,0 +1,20 @@
+function delayBlocking() {
+  alert("Fetching user data");
+  return "Blocking delay completed ";
+}
+
+console.log("Start blocking delay");
+console.log(delayBlocking());
+console.log("This message is blocked untill delay is complete");
+
+function nonBlockingDelay() {
+  setTimeout(() => {
+    console.log("Non blocking delay completed after 2s !");
+  }, 2000);
+}
+
+console.log("fetch Start");
+
+nonBlockingDelay();
+
+console.log("fetch End");
