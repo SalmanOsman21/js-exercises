fdacd8f7ba2a700104dc43609e71e4b68094455f
SalmanOsman21 <salmanosman281@gmail.com>
2026-07-26 20:21:09 +0300

    Exercise 31

---
+++ Exercise 31/main.js
@@ -0,0 +1,16 @@
+async function getUser() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+    if (!response.ok) {
+      throw new Error(`HTTP Error status : ${response.status}`);
+    }
+
+    const users = await response.json();
+    console.log("Users", users);
+  } catch (error) {
+    console.log("Error", error);
+  }
+}
+
+getUser();
